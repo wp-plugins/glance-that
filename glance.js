f5d2424312820dcf065465d91aa9a018904308be
UaMV <UaMV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-11 01:04:33 +0000

    minor css fix
    git-svn-id: https://plugins.svn.wordpress.org/glance-that/tags/1.3@873151 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ glance.js
@@ -0,0 +1,51 @@
+/**
+ * Theme cards for displaying excerpt and post content.
+ */
+jQuery(document).ready(function( $ ) {
+
+	$('#visible-icon').click(
+		function() {
+			if( $('#iconlist').is(':visible') ) {
+				$('#iconlist').hide();
+			} else {
+				$('#iconlist').css('display','block');
+				$('#dashboard_right_now .inside').css('overflow','visible');
+			}
+		});
+
+	$('#gt-item').change(
+		function() {
+			$gtselection = $(this).find(':selected');
+
+			if ( '' != $gtselection.attr('data-dashicon') ) {
+
+				$gticon = $('#iconlist').find('div[data-dashicon="'+$gtselection.attr('data-dashicon')+'"]');
+
+				$('#visible-icon').attr('alt',$gticon.attr('alt'));
+				$('#visible-icon').removeClass();
+				$('#visible-icon').addClass($gticon.attr('class'));
+				$('input[data-dashicon="selected"]').attr('value',$gticon.attr('alt'));
+			}
+
+		});
+
+	$('.dashicon-option').click(
+		function() {
+			$('#visible-icon').attr('alt',$(this).attr('alt'));
+			$('#visible-icon').removeClass();
+			$('#visible-icon').addClass($(this).attr('class'));
+			$('input[data-dashicon="selected"]').attr('value',$(this).attr('alt'));
+			$('#iconlist').hide();
+		});
+
+	$('#show-gt-form').click(
+		function() {
+			$('#gt-form').css('display','block');
+			$(this).css('display','none');
+			});
+
+	$('#remove-gt-item').click(
+		function() {
+			$('#gt-form').attr('action','index.php?action=remove-gt-item');
+		});
+});
